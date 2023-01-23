1404041ff6bd62890c97815afa8c214b0b47965d
МАРК <markpan07@mail.ru>
2023-01-23 11:33:38 +0300

    Homework is done!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -23,20 +25,52 @@ public class Main {
 
     private static void task1() {
         System.out.println("Task 1");
-
+        int[] costs = generateRandomArray();
+        int totalCost = 0;
+        for (int i = 0; i < costs.length; i++) {
+            totalCost = totalCost + costs[i];
+        }
+        System.out.println("Total costs are equal to " + totalCost);
     }
 
     private static void task2() {
         System.out.println("Task 2");
-
+        int[] costs = generateRandomArray();
+        int min = 201_000;
+        int max = 0;
+        System.out.println(Arrays.toString(costs));
+        System.out.println();
+        for (int i = 0; i < costs.length; i++) {
+            if (costs[i] > max) {
+                max = costs[i];
+            } else if (costs[i] < min) {
+                min = costs[i];
+            }
+        }
+        System.out.println("Minimum is " + min);
+        System.out.println("Maximum is " + max);
     }
 
     private static void task3() {
         System.out.println("Task 3");
-
+        int[] costs = generateRandomArray();
+        double averege;
+        int totalCost = 0;
+        for (int i = 0; i < costs.length; i++) {
+            totalCost = totalCost + costs[i];
+        }
+        averege = (double) totalCost / costs.length;
+        System.out.println("Total costs: " + totalCost);
+        System.out.println("Total days: " + costs.length);
+        System.out.printf("Averege is: %.2f", averege);
+        System.out.println();
     }
 
     private static void task4() {
         System.out.println("Task 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
